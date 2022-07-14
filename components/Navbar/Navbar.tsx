6787fe11f047e8d2ab275fc644f8a4640587d533
Kaisar Ahmmed <kaisar123k@gmail.com>
2022-07-14 12:08:00 +0600

    initial app

---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,21 @@
+import { Flex, Image } from "@chakra-ui/react";
+import React from "react";
+
+type Props = {};
+
+const Navbar = (props: Props) => {
+    return (
+        <Flex bg="white" height="44px" padding="6px 12px">
+            <Flex align="center">
+                <Image src="/images/redditFace.svg" height="30px" />
+                <Image
+                    src="/images/redditText.svg"
+                    height="46px"
+                    display={{ base: "none", md: "unset" }}
+                />
+            </Flex>
+        </Flex>
+    );
+};
+
+export default Navbar;
